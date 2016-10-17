0cc2328746ecd5f98e037de1bb65dc8bdc6c958a
mcdatasoft <mg.cuadros@hotmail.com>
2016-10-16 21:17:04 -0500

    cambios 9:16

---
+++ appharbor/Controllers/clientesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace appharbor.Controllers
+{
+    public class clientesController : ApiController
+    {
+        sapEntities dataserver = new sapEntities();
+
+        [Route("api/clientes")]
+        [HttpGet]
+        public IEnumerable<OCRD_Select_Result> listarclientes() {
+            
+            return dataserver.OCRD_Select(null,null,null).AsEnumerable();
+        }
+        
+    }
+}
